6389c068c690cf2bb24d2719388fc871d43b44d6
usermuser <lwrdrstv@gmail.com>
2020-02-13 14:17:55 +0500

    bubble_sort - found bug

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,15 +1,7 @@
-# alist = [3, 2, 1]
-# alist = [6,1,7,8,9,3,5,4,2]
-alist = [6, 3, 5, 4, 2]
-
-def bsort(a):
-    for i in range(len(a)):
-        if a[i] > a[i+1]:
-           a[i], a[i+1] = a[i+1], a[i]
-    return a
+"""code examples for bubble sort algorithm"""
 
 
-def bsort2(a):
+def bsort1(a):
     print('[+] Started BubbleSort for: {}'.format(alist))
     for i in range(len(a)-1):
         for j in range(len(a)):
@@ -21,12 +13,12 @@ def bsort2(a):
     return print('[+] Result {}'.format(a))
 
 
-# bsort2(alist)
+# alist = [6, 3, 5, 4, 2]
+# bsort1(alist)
 
 
-# alist = [6,3,5,4,2]
 def short_bubble_sort(alist):
-    print('\n[+] short_bubble_sort started on: {}'.format(alist))
+    print('\n[+] short_bubble_sort started for: {}'.format(alist))
     exchanges = True              # to catch sorted state asap
     exch_counter = 0
     passnum = len(alist)-1
@@ -42,4 +34,8 @@ def short_bubble_sort(alist):
         passnum -= 1
     return print('[+] result: {}, total exchanges:{}'.format(alist, exch_counter))
 
-short_bubble_sort(alist)
+# BUG = only one swap is performed for one pass
+# alist = [6,3,5,4,2]
+l1 = [6, 1, 7, 8, 9, 3, 5, 4, 2]
+l2 = []
+short_bubble_sort(l1)
