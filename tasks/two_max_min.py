bab0f3ebaa7844e2a35c861aab3a4bfd3b31326d
usermuser <lwrdrstv@gmail.com>
2020-06-26 22:42:54 +0300

    two_max_min task: added function to solve algorithm

---
+++ tasks/two_max_min.py
@@ -0,0 +1,37 @@
+"""Задача:В массиве чисел найти два максимальных элемента.
+При этом они могут быть равны между собой или не равны.
+
+input: [4,7,2,6,9]
+output: 7,9
+
+input: [2,4,6,8,1,3,5,7,8]
+output: 8,8
+
+#1 Оба максимума ищутся в одном цикле
+      Время     Память     Модификация
+#1    O(n)      O(1)        No
+"""
+
+data1 = [4, 7, 2, 6, 9]
+data2 = [2, 4, 6, 8, 1, 3, 5, 7, 8]
+
+
+def two_max(data):
+    length = len(data)
+    max1 = 0
+    max2 = 1  # max2 must be bigger than max1
+    if data[max1] > data[max2]:
+        max1 = 1
+        max2 = 0
+    for i in range(2, length):
+        if data[i] > max1:
+            max1 = i
+        if max2 > max1:
+            max1, max2 = max2, max1
+
+    return data[max1], data[max2]
+
+
+if __name__ == '__main__':
+    print(two_max(data1))
+    print(two_max(data2))
